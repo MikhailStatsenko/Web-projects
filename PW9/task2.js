b91bd36760f5c6264c4eac8604c5b93151885027
mikhail <statsenko.mikhail.a@gmail.com>
2022-11-09 22:08:26 +0300

    Task 9 done

---
+++ PW9/task2.js
@@ -0,0 +1,17 @@
+var login, password;
+login = prompt("Введите логин");
+
+if (login == "Админ") {
+  password = prompt("Введите пароль");
+  if (password == "Я главный") {
+    alert("Здравствуйте!");
+  } else if (!password) {
+    alert("Отменено");
+  } else {
+    alert("Неверный пароль");
+  }
+} else if (!login) {
+  alert("Отменено");
+} else {
+  alert("Я вас не знаю");
+}
---
+++ PW9/task34.js
@@ -0,0 +1,45 @@
+const button = document.querySelector(".buttons");
+const i = button.querySelector(".fa-heart");
+const block = document.querySelector(".page");
+const drawnHearts = document.querySelector(".generated-hearts");
+const btn = document.querySelector(".button.button");
+
+btn.onclick = function () {
+  if (btn.style.background == "rgb(229, 229, 229)") {
+    btn.style.background = "rgb(255, 213, 213)";
+    btn.backgroundColor = "red";
+    i.style.color = "rgb(255, 83, 83)";
+  } else {
+    btn.style.background = "rgb(229, 229, 229)";
+    i.style.color = "rgb(104, 104, 104)";
+  }
+};
+
+button.addEventListener("click", () => {
+  if (button.classList.contains("flag-false")) {
+    button.classList.remove("flag-false");
+    button.classList.add("flag-true");
+    block.addEventListener("mousemove", drawHeart);
+  } else {
+    button.classList.remove("flag-true");
+    button.classList.add("flag-false");
+    block.removeEventListener("mousemove", drawHeart);
+    drawnHearts.innerHTML = "";
+  }
+});
+
+function drawHeart(event) {
+  let x = event.pageX;
+  let y = event.pageY;
+
+  let heart = document.createElement("i");
+  heart.className = "fa fa-heart";
+  heart.style = `
+    left: ${x}px;
+    top: ${y - 25}px;
+    font-size: 25px;
+    position: absolute;
+    color: rgb(255, 83, 83);
+    `;
+  drawnHearts.append(heart);
+}
